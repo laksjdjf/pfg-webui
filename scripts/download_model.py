9e1166d7f4e2737ff9519fdad670d65fa5e904b9
laksjdjf <aikasama@hotmail.co.jp>
2023-02-24 15:51:06 +0900

    Update download_model.py

--- scripts/download_model.py
+++ scripts/download_model.py
@@ -1,6 +1,7 @@
 #このコードはhttps://github.com/kohya-ss/sd-scripts/blob/main/finetune/tag_images_by_wd14_tagger.pyを参考にしていますというかパクっています。
 
 from huggingface_hub import hf_hub_download
+import os
 
 DEFAULT_WD14_TAGGER_REPO = 'SmilingWolf/wd-v1-4-vit-tagger-v2'
 FILES = ["keras_metadata.pb", "saved_model.pb"]
@@ -9,7 +10,7 @@ SUB_DIR_FILES = ["variables.data-00000-of-00001", "variables.index"]
 
 def download(path):
     model_dir = os.path.join(path, "wd-v1-4-vit-tagger-v2")
-    if not os.path.exists():
+    if not os.path.exists(model_dir):
         print(f"downloading wd14 tagger model from hf_hub. id: {DEFAULT_WD14_TAGGER_REPO}")
         for file in FILES:
             hf_hub_download(DEFAULT_WD14_TAGGER_REPO, file, cache_dir=model_dir, force_download=True, force_filename=file)
