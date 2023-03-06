b7738bdc8aca297a2c4dc38d2c80222ee2085d3a
laksjdjf <aikasama@hotmail.co.jp>
2023-03-06 14:22:42 +0900

    support onnx

--- scripts/pfg-webui.py
+++ scripts/pfg-webui.py
@@ -13,11 +13,8 @@ from modules.script_callbacks import CFGDenoiserParams, on_cfg_denoiser
 from modules.processing import StableDiffusionProcessing
 
 from scripts.dbimutils import smart_imread_pil, smart_24bit, make_square, smart_resize
-from scripts.download_model import download, TAGGER_DIR
+from scripts.download_model import download, TAGGER_DIR, ONNX_FILE
 from PIL import Image
-
-import tensorflow as tf
-from tensorflow.keras.models import load_model, Model
 
 import os
 
@@ -49,8 +46,10 @@ class Script(scripts.Script):
                     pfg_path = gr.Dropdown(self.model_list, label="pfg model", value = None)
                 with gr.Row():
                     pfg_num_tokens = gr.Slider(minimum=0, maximum=20, step=1.0, value=10.0, label="pfg num tokens")
+                with gr.Row():
+                    use_onnx = gr.Checkbox(value=False, label="use onnx")
                     
-        return enabled, image, pfg_scale, pfg_path, pfg_num_tokens
+        return enabled, image, pfg_scale, pfg_path, pfg_num_tokens, use_onnx
     
     #wd-14-taggerの推論関数
     def infer(self, img:Image):
@@ -59,8 +58,13 @@ class Script(scripts.Script):
         img = make_square(img, 448)
         img = smart_resize(img, 448)
         img = img.astype(np.float32)
-        probs = self.tagger(np.array([img]), training=False)
-        return torch.tensor(probs.numpy()).squeeze(0).cpu()
+        if self.use_onnx:
+            print("inferencing by onnx model.")
+            probs = self.tagger.run([self.tagger.get_outputs()[0].name],{self.tagger.get_inputs()[0].name: np.array([img])})[0]
+        else:
+            print("inferencing by tensorflow model.")
+            probs = self.tagger(np.array([img]), training=False).numpy()
+        return torch.tensor(probs).squeeze(0).cpu()
     
     #CFGのdenoising step前に起動してくれるらしい。
     def denoiser_callback(self, params: CFGDenoiserParams):
@@ -72,7 +76,7 @@ class Script(scripts.Script):
             uncond = params.uncond
 
             #(768,)
-            pfg_feature = self.infer(self.image)
+            pfg_feature = self.pfg_feature
 
             #(768,) -> (768 * num_tokens, )
             pfg_cond = self.weight @ pfg_feature + self.bias
@@ -91,7 +95,7 @@ class Script(scripts.Script):
                 print(f"Apply pfg num_tokens:{self.pfg_num_tokens}(this message will be duplicated)")
                                      
 
-    def process(self, p: StableDiffusionProcessing, enabled:bool, image: Image, pfg_scale:float, pfg_path: str, pfg_num_tokens:int):
+    def process(self, p: StableDiffusionProcessing, enabled:bool, image: Image, pfg_scale:float, pfg_path: str, pfg_num_tokens:int, use_onnx):
         
         self.enabled = enabled
         if not self.enabled:
@@ -105,19 +109,29 @@ class Script(scripts.Script):
         self.weight = pfg_weight["pfg_linear.weight"].cpu() #大した計算じゃないのでcpuでいいでしょう
         self.bias = pfg_weight["pfg_linear.bias"].cpu()
 
-        if not hasattr(self, 'tagger'):
-            #なんもいみわかっとらんけどこれしないとVRAMくわれる。対応するバージョンもよくわからない
-            physical_devices = tf.config.list_physical_devices('GPU')
-            if len(physical_devices) > 0:
-                for device in physical_devices:
-                    tf.config.experimental.set_memory_growth(device, True)
-                    print('{} memory growth: {}'.format(device, tf.config.experimental.get_memory_growth(device)))
+        if not hasattr(self, 'tagger') or self.use_onnx != use_onnx:
+            if use_onnx:
+                import onnxruntime
+                self.tagger = onnxruntime.InferenceSession(os.path.join(CURRENT_DIRECTORY, ONNX_FILE),providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
             else:
-                print("Not enough GPU hardware devices available")
+                import tensorflow as tf
+                from tensorflow.keras.models import load_model, Model
+                #なんもいみわかっとらんけどこれしないとVRAMくわれる。対応するバージョンもよくわからない
+                physical_devices = tf.config.list_physical_devices('GPU')
+                if len(physical_devices) > 0:
+                    for device in physical_devices:
+                        tf.config.experimental.set_memory_growth(device, True)
+                        print('{} memory growth: {}'.format(device, tf.config.experimental.get_memory_growth(device)))
+                else:
+                    print("Not enough GPU hardware devices available")
 
-            self.tagger = load_model(os.path.join(CURRENT_DIRECTORY, TAGGER_DIR))
-            self.tagger = Model(self.tagger.layers[0].input, self.tagger.layers[-3].output) #最終層手前のプーリング層の出力を使う
-                                     
+                self.tagger = load_model(os.path.join(CURRENT_DIRECTORY, TAGGER_DIR))
+                self.tagger = Model(self.tagger.layers[0].input, self.tagger.layers[-3].output) #最終層手前のプーリング層の出力を使う
+        
+        self.use_onnx = use_onnx
+        
+        self.pfg_feature = self.infer(self.image)
+        
         if not hasattr(self, 'callbacks_added'):
             on_cfg_denoiser(self.denoiser_callback)
             self.callbacks_added = True
